c965663020009003329097091a61f0ac360e8f39
Orphy-a <jaeyoung0560@gmail.com>
2025-06-02 12:37:43 +0900

    설정 페이지 내 정보 화면 구현

---
+++ zylo-react/src/components/setting/Profile.jsx
@@ -0,0 +1,65 @@
+import { faPortrait } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { useTheme } from "../../contexts/ThemeContext";
+
+const Profile = () => {
+  const { toggled, toggle } = useTheme();
+
+  return (
+    <>
+      <div className="content-wrapper">
+        <form>
+          <div className="setting-content-title">
+            <span>
+              <FontAwesomeIcon icon={faPortrait} className="menu-icon" />
+            </span>
+            프로필 수정
+          </div>
+          <div className="setting-content-subtitle">
+            대표 프로필과 이름을 수정 하실 수 있습니다.
+          </div>
+          <table
+            className={`myinfo-table ${toggled ? "dark" : "light"}`}
+            border="1"
+          >
+            <tbody>
+              <tr>
+                <td
+                  className={`myinfo-table-label ${toggled ? "dark" : "light"}`}
+                >
+                  프로필사진
+                </td>
+                <td>
+                  <img src="" alt="" className="profile-photo" />
+                  <br />
+                  <button className={toggled ? "dark" : "light"}>
+                    사진변경
+                  </button>
+                  <button className={toggled ? "dark" : "light"}>삭제</button>
+                </td>
+              </tr>
+              <tr>
+                <td
+                  className={`myinfo-table-label ${toggled ? "dark" : "light"}`}
+                >
+                  이름
+                </td>
+                <td>
+                  <input type="text" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <input
+            type="submit"
+            className={`myinfo-table-submit ${toggled ? "dark" : "light"}`}
+            value="저장"
+          ></input>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
--- zylo-react/src/routers/routers.jsx
+++ zylo-react/src/routers/routers.jsx
@@ -2,9 +2,11 @@ import { DrivePage } from "../pages/DrivePage";
 import { createBrowserRouter } from "react-router-dom";
 import { MainPage } from "../pages/main/MainPage";
 import MyInfoPage from "../pages/setting/MyInfoPage";
+import ProfilePage from "../pages/setting/ProfilePage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <MainPage /> },
   { path: "/drive", element: <DrivePage /> },
   { path: "/setting/MyInfo", element: <MyInfoPage /> },
+  { path: "/setting/Profile", element: <ProfilePage /> },
 ]);
