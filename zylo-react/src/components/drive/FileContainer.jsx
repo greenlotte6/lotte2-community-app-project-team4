963ef14646ab483fdb7c4757ae255b47b3179543
id3ntity99 <jas06113@gmail.com>
2025-06-03 16:25:05 +0900

    파일 브라우저 UI 추가

--- zylo-react/src/components/drive/FileContainer.jsx
+++ zylo-react/src/components/drive/FileContainer.jsx
@@ -2,41 +2,41 @@ import { faPlus, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { useTheme } from "../../contexts/ThemeContext";
+import { SearchBox } from "./SearchBox";
 
 export const FileContainer = () => {
   const { toggled } = useTheme();
   return (
-    <>
-      <div id="drive">
-        <div id="grid-container">
-          <section id="file-grid">
-            <div className="file-container">
-              <Link to="" className={toggled ? "file dark" : "file"}></Link>
-            </div>
-            <div className="file-container">
-              <Link to="" className={toggled ? "file dark" : "file"}></Link>
-            </div>
-            <div className="file-container">
-              <Link to="" className={toggled ? "file dark" : "file"}></Link>
-            </div>
-            <div className="file-container">
-              <Link to="" className={toggled ? "file dark" : "file"}></Link>
-            </div>
-            <div className="file-container">
-              <Link to="" className={toggled ? "file dark" : "file"}></Link>
-            </div>
-            <div className="file-container">
-              <Link to="" className={toggled ? "file dark" : "file"}></Link>
-            </div>
-          </section>
-        </div>
-        <button id="more" className={toggled ? "dark" : "light"}>
-          더보기 <FontAwesomeIcon icon={faPlus} />
-        </button>
-        <button id="upload-btn" className={toggled ? "dark" : "light"}>
-          업로드 <FontAwesomeIcon icon={faUpload} />
-        </button>
+    <div id="drive">
+      <SearchBox />
+      <div id="grid-container">
+        <section id="file-grid">
+          <div className="file-container">
+            <Link to="" className={toggled ? "file dark" : "file"}></Link>
+          </div>
+          <div className="file-container">
+            <Link to="" className={toggled ? "file dark" : "file"}></Link>
+          </div>
+          <div className="file-container">
+            <Link to="" className={toggled ? "file dark" : "file"}></Link>
+          </div>
+          <div className="file-container">
+            <Link to="" className={toggled ? "file dark" : "file"}></Link>
+          </div>
+          <div className="file-container">
+            <Link to="" className={toggled ? "file dark" : "file"}></Link>
+          </div>
+          <div className="file-container">
+            <Link to="" className={toggled ? "file dark" : "file"}></Link>
+          </div>
+        </section>
       </div>
-    </>
+      <button id="more" className={toggled ? "dark" : "light"}>
+        더보기 <FontAwesomeIcon icon={faPlus} />
+      </button>
+      <button id="upload-btn" className={toggled ? "dark" : "light"}>
+        업로드 <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </div>
   );
 };
