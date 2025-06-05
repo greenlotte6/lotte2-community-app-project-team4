76a18f1d1ee26cf23a56c555102b71eaf2eb0ca7
id3ntity99 <jas06113@gmail.com>
2025-06-05 11:30:49 +0900

    다이렉트 메시지 페이지 다크모드 적용 완료

--- zylo-react/src/components/message/mainMessage/ChatRoomMessage.jsx
+++ zylo-react/src/components/message/mainMessage/ChatRoomMessage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useTheme } from "../../../contexts/ThemeContext";
 
 export const ChatRoomMessage = () => {
+  const { toggled } = useTheme();
   return (
     <>
       <div className="chat-room-messages">
@@ -14,11 +16,15 @@ export const ChatRoomMessage = () => {
             alt="Diana"
           />
           <div className="message-wrapper">
-            <div className="chat-room-meta">
+            <div className={toggled ? "chat-room-meta dark" : "chat-room-meta"}>
               <strong>Diana</strong>
               <span className="time">5:36 AM</span>
             </div>
-            <div className="chat-room-content">
+            <div
+              className={
+                toggled ? "chat-room-content dark" : "chat-room-content"
+              }
+            >
               <p>Professional!</p>
             </div>
             <div className="unread-count">3</div>
@@ -33,11 +39,15 @@ export const ChatRoomMessage = () => {
             alt="Robert"
           />
           <div className="message-wrapper">
-            <div className="chat-room-meta">
+            <div className={toggled ? "chat-room-meta dark" : "chat-room-meta"}>
               <strong>Robert</strong>
               <span className="time">8:53 AM</span>
             </div>
-            <div className="chat-room-content">
+            <div
+              className={
+                toggled ? "chat-room-content dark" : "chat-room-content"
+              }
+            >
               <div className="attachment">
                 <img src="/images/message/cat.jpg" alt="dashboard" />
                 <p>This new dashboard page. What do you think?</p>
@@ -50,11 +60,15 @@ export const ChatRoomMessage = () => {
         {/* 내 메시지 */}
         <div className="chat-room-message self">
           <div className="message-wrapper">
-            <div className="chat-room-meta">
+            <div className={toggled ? "chat-room-meta dark" : "chat-room-meta"}>
               <strong>You</strong>
               <span className="time">1:36 PM</span>
             </div>
-            <div className="chat-room-content">
+            <div
+              className={
+                toggled ? "chat-room-content dark" : "chat-room-content"
+              }
+            >
               <p>Wow it looks amazing.</p>
             </div>
             <div className="unread-count">3</div>
@@ -70,11 +84,15 @@ export const ChatRoomMessage = () => {
             alt="Diana"
           />
           <div className="message-wrapper">
-            <div className="chat-room-meta">
+            <div className={toggled ? "chat-room-meta dark" : "chat-room-meta"}>
               <strong>Diana</strong>
               <span className="time">5:36 AM</span>
             </div>
-            <div className="chat-room-content">
+            <div
+              className={
+                toggled ? "chat-room-content dark" : "chat-room-content"
+              }
+            >
               <p>Professional!</p>
             </div>
             <div className="unread-count">3</div>
@@ -89,11 +107,15 @@ export const ChatRoomMessage = () => {
             alt="Robert"
           />
           <div className="message-wrapper">
-            <div className="chat-room-meta">
+            <div className={toggled ? "chat-room-meta dark" : "chat-room-meta"}>
               <strong>Robert</strong>
               <span className="time">8:53 AM</span>
             </div>
-            <div className="chat-room-content">
+            <div
+              className={
+                toggled ? "chat-room-content dark" : "chat-room-content"
+              }
+            >
               <div className="attachment">
                 <img src="/images/message/cat.jpg" alt="dashboard" />
                 <p>This new dashboard page. What do you think?</p>
@@ -106,11 +128,15 @@ export const ChatRoomMessage = () => {
         {/* 내 메시지 */}
         <div className="chat-room-message self">
           <div className="message-wrapper">
-            <div className="chat-room-meta">
+            <div className={toggled ? "chat-room-meta dark" : "chat-room-meta"}>
               <strong>You</strong>
               <span className="time">1:36 PM</span>
             </div>
-            <div className="chat-room-content">
+            <div
+              className={
+                toggled ? "chat-room-content dark" : "chat-room-content"
+              }
+            >
               <p>Wow it looks amazing.</p>
             </div>
             <div className="unread-count">3</div>
--- zylo-react/src/components/message/mainMessage/MessageSidebar.jsx
+++ zylo-react/src/components/message/mainMessage/MessageSidebar.jsx
@@ -1,126 +1,132 @@
-import React from 'react'
+import React from "react";
+import { useTheme } from "../../../contexts/ThemeContext";
 
 export const MessageSidebar = () => {
+  const { toggled } = useTheme();
   return (
-        <>
-         <section className="Message">
-          <header className="Message-header">
-            <h1>메세지</h1>
-            <p>125 메세지 - 5 안읽음</p>
-            <input type="text" placeholder="검색" />
-          </header>
+    <>
+      <section className={toggled ? "Message dark" : "Message"}>
+        <header className="Message-header">
+          <h1>메세지</h1>
+          <p>125 메세지 - 5 안읽음</p>
+          <input type="text" placeholder="검색" />
+        </header>
 
-          {/* 온라인 사용자 */}
-          <div className="online-now">
-            <div className="online-header">
-              <p>접속 중</p>
-              <button id="showAllBtn">모두보기</button>
+        {/* 온라인 사용자 */}
+        <div className="online-now">
+          <div className="online-header">
+            <p>접속 중</p>
+            <button id="showAllBtn">모두보기</button>
+          </div>
+          <div className="avatars" id="avatarList">
+            {[...Array(4)].map((_, i) => (
+              <div className="avatar-wrapper" key={i}>
+                <img
+                  src="/images/message/avatars.png"
+                  alt="user"
+                  className="avatar"
+                />
+                <span className="online-indicator"></span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* 채팅 목록 영역 */}
+        <section className="group-section">
+          {/* 다이렉트 메시지 */}
+          <div className="project-group">
+            <div className="project-header" data-toggle="group1">
+              <h5 className="project-title">다이렉트 메세지</h5>
+              <span className="toggle-icon">▼</span>
             </div>
-            <div className="avatars" id="avatarList">
-              {[...Array(4)].map((_, i) => (
-                <div className="avatar-wrapper" key={i}>
-                  <img src="/images/message/avatars.png" alt="user" className="avatar" />
+            <ul className="chat-list collapsible" id="group1">
+              <li className={toggled ? "chat-item dark" : "chat-item"}>
+                <div className="avatar-wrapper">
+                  <img
+                    className="avatar"
+                    src="/images/message/avatars.png"
+                    alt="Robert Fox"
+                  />
                   <span className="online-indicator"></span>
                 </div>
-              ))}
-            </div>
+                <div className="chat-info">
+                  <div className="chat-name">Robert Fox</div>
+                  <div className="chat-sub">Typing...</div>
+                </div>
+                <div className="chat-meta">
+                  <span className="chat-time">10:52 AM</span>
+                  <span className="chat-status typing"></span>
+                </div>
+              </li>
+
+              <li className={toggled ? "chat-item dark" : "chat-item"}>
+                <div className="avatar-wrapper">
+                  <img
+                    className="avatar"
+                    src="/images/message/avatars.png"
+                    alt="Devon Lane"
+                  />
+                </div>
+                <div className="chat-info">
+                  <div className="chat-name">Devon Lane</div>
+                  <div className="chat-sub">If we so can, tomorrow</div>
+                </div>
+                <div className="chat-meta">
+                  <span className="chat-time">04:25 PM</span>
+                  <span className="chat-status read"></span>
+                </div>
+              </li>
+            </ul>
           </div>
 
-          {/* 채팅 목록 영역 */}
-          <section className="group-section">
-            {/* 다이렉트 메시지 */}
-            <div className="project-group">
-              <div className="project-header" data-toggle="group1">
-                <h5 className="project-title">다이렉트 메세지</h5>
-                <span className="toggle-icon">▼</span>
-              </div>
-              <ul className="chat-list collapsible" id="group1">
-                <li className="chat-item">
-                  <div className="avatar-wrapper">
-                    <img
-                      className="avatar"
-                      src="/images/message/avatars.png"
-                      alt="Robert Fox"
-                    />
-                    <span className="online-indicator"></span>
-                  </div>
-                  <div className="chat-info">
-                    <div className="chat-name">Robert Fox</div>
-                    <div className="chat-sub">Typing...</div>
-                  </div>
-                  <div className="chat-meta">
-                    <span className="chat-time">10:52 AM</span>
-                    <span className="chat-status typing"></span>
-                  </div>
-                </li>
-
-                <li className="chat-item">
-                  <div className="avatar-wrapper">
-                    <img
-                      className="avatar"
-                      src="/images/message/avatars.png"
-                      alt="Devon Lane"
-                    />
-                  </div>
-                  <div className="chat-info">
-                    <div className="chat-name">Devon Lane</div>
-                    <div className="chat-sub">If we so can, tomorrow</div>
-                  </div>
-                  <div className="chat-meta">
-                    <span className="chat-time">04:25 PM</span>
-                    <span className="chat-status read"></span>
-                  </div>
-                </li>
-              </ul>
+          {/* 그룹 채팅 */}
+          <div className="project-group">
+            <div className="project-header" data-toggle="group2">
+              <h5 className="project-title">그룹 채팅</h5>
+              <span className="toggle-icon">▼</span>
             </div>
+            <ul className="chat-list collapsible" id="group2">
+              <li className={toggled ? "chat-item dark" : "chat-item"}>
+                <div className="avatar-wrapper">
+                  <img
+                    className="avatar"
+                    src="/images/message/avatars.png"
+                    alt="Kristin Watson"
+                  />
+                  <span className="online-indicator"></span>
+                </div>
+                <div className="chat-info">
+                  <div className="chat-name">Kristin Watson</div>
+                  <div className="chat-sub">If we so can, tomorrow</div>
+                </div>
+                <div className="chat-meta">
+                  <span className="chat-time">3:12 AM</span>
+                  <span className="chat-status unread">3</span>
+                </div>
+              </li>
 
-            {/* 그룹 채팅 */}
-            <div className="project-group">
-              <div className="project-header" data-toggle="group2">
-                <h5 className="project-title">그룹 채팅</h5>
-                <span className="toggle-icon">▼</span>
-              </div>
-              <ul className="chat-list collapsible" id="group2">
-                <li className="chat-item">
-                  <div className="avatar-wrapper">
-                    <img
-                      className="avatar"
-                      src="/images/message/avatars.png"
-                      alt="Kristin Watson"
-                    />
-                    <span className="online-indicator"></span>
-                  </div>
-                  <div className="chat-info">
-                    <div className="chat-name">Kristin Watson</div>
-                    <div className="chat-sub">If we so can, tomorrow</div>
-                  </div>
-                  <div className="chat-meta">
-                    <span className="chat-time">3:12 AM</span>
-                    <span className="chat-status unread">3</span>
-                  </div>
-                </li>
-
-                <li className="chat-item">
-                  <div className="avatar-wrapper">
-                    <img
-                      className="avatar"
-                      src="/images/message/avatars.png"
-                      alt="Courtney Henry"
-                    />
-                  </div>
-                  <div className="chat-info">
-                    <div className="chat-name">Courtney Henry</div>
-                    <div className="chat-sub">If we so can, tomorrow</div>
-                  </div>
-                  <div className="chat-meta">
-                    <span className="chat-time">1:03 AM</span>
-                    <span className="chat-status read">3</span>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </section>
+              <li className={toggled ? "chat-item dark" : "chat-item"}>
+                <div className="avatar-wrapper">
+                  <img
+                    className="avatar"
+                    src="/images/message/avatars.png"
+                    alt="Courtney Henry"
+                  />
+                </div>
+                <div className="chat-info">
+                  <div className="chat-name">Courtney Henry</div>
+                  <div className="chat-sub">If we so can, tomorrow</div>
+                </div>
+                <div className="chat-meta">
+                  <span className="chat-time">1:03 AM</span>
+                  <span className="chat-status read">3</span>
+                </div>
+              </li>
+            </ul>
+          </div>
         </section>
+      </section>
     </>
-  )
-}
+  );
+};
