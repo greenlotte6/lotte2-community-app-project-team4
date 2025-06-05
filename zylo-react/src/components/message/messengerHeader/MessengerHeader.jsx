8c21634f0bda43e4cf5e8a8542d72e9c4d92d058
id3ntity99 <jas06113@gmail.com>
2025-06-05 10:15:37 +0900

    Modified css

--- zylo-react/src/components/message/messengerHeader/MessengerHeader.jsx
+++ zylo-react/src/components/message/messengerHeader/MessengerHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useTheme } from "../../../contexts/ThemeContext";
 import { ThemeToggle } from "./ThemeToggle";
 import { MoreMenu } from "./MoreMenu";
@@ -7,15 +7,12 @@ import { SearchBar } from "./SearchBar";
 
 export const MessengerHeader = ({ chatroomElements }) => {
   const { toggled } = useTheme();
-  const [isSearchOpen, setIsSearchOpen] = useState(false); // 🔵 상태 추가
 
   return (
     <header id="content-header" className={toggled ? "dark" : "light"}>
       {chatroomElements}
       <nav>
         <ThemeToggle />
-        {isSearchOpen && <SearchBar onClose={() => setIsSearchOpen(false)} />} {/* 🔵 검색창 조건부 렌더 */}
-        <MoreMenu onOpenSearch={() => setIsSearchOpen(true)} /> {/* 🔵 props로 전달 */}
         <ProfileLink />
       </nav>
     </header>
