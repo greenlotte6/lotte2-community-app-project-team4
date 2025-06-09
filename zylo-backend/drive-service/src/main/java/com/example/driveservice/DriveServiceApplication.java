301864ccc6b3fdee42c01c83433e6fac29d8c7ac
id3ntity99 <jas06113@gmail.com>
2025-06-09 11:23:38 +0900

    Gradle multi module 프로젝트 생성 완료

---
+++ zylo-backend/drive-service/src/main/java/com/example/driveservice/DriveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.driveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DriveServiceApplication.class, args);
+  }
+
+}
