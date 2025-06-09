e2ac2068d9caf62258404e8b2a058db4c911a28e
id3ntity99 <jas06113@gmail.com>
2025-06-09 11:36:23 +0900

    Deleted temp comment

--- zylo-backend/drive-service/src/main/java/com/example/driveservice/DriveServiceApplication.java
+++ zylo-backend/drive-service/src/main/java/com/example/driveservice/DriveServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DriveServiceApplication {
 
-  //TODO: Temp comment for checking CI/CD pipeline. Delete this after checking
   public static void main(String[] args) {
     SpringApplication.run(DriveServiceApplication.class, args);
   }
