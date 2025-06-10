5ee99349848ff19c71ad260e379881e31baa189f
junyoung1125 <wnsdud060451@gmail.com>
2025-06-10 17:20:57 +0900

    ListPage수정

--- zylo-react/src/pages/article/ListPage.jsx
+++ zylo-react/src/pages/article/ListPage.jsx
@@ -1,7 +1,7 @@
 import PostList from "../../components/article/PostList";
 import { BasicLayout } from "../../layouts/BasicLayout";
 import { Pagination }from "../../components/article/Pagination";
-import "../../styles/article/postList.css";
+import "../../styles/article/PostList.css";
 import { Link } from "react-router-dom";
 import { SideMenu } from "../../components/article/SideMenu";
 
