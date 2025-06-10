6d1ca1b1d2fe74343f3e70bc49f874e48944f692
sohyeon <sohyoun26@gmail.com>
2025-06-10 18:09:25 +0900

    add

--- zylo-react/vite.config.js
+++ zylo-react/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // 'global' 변수를 window 객체로 정의하여 브라우저 환경에서 사용 가능하게 함
+    global: "window",
+  },
+});
