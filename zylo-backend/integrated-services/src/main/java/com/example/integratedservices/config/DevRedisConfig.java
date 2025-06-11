c96bbab7d2ecf9a0ac13f6224b50e5c556b1b9f7
id3ntity99 <jas06113@gmail.com>
2025-06-11 16:35:29 +0900

    redis config added

--- zylo-backend/integrated-services/src/main/java/com/example/integratedservices/config/DevRedisConfig.java
+++ zylo-backend/integrated-services/src/main/java/com/example/integratedservices/config/DevRedisConfig.java
@@ -3,13 +3,15 @@ package com.example.integratedservices.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig {
+@Profile("dev")
+public class DevRedisConfig {
 
   @Value("${spring.data.redis.host}")
   private String host;
