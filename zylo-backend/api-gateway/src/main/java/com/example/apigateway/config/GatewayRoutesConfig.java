c14c232e05d51d57c6c36cef22a9456a55494d23
id3ntity99 <jas06113@gmail.com>
2025-06-12 14:59:15 +0900

    API gateway 구축 중

--- zylo-backend/api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
+++ zylo-backend/api-gateway/src/main/java/com/example/apigateway/config/GatewayRoutesConfig.java
@@ -1,5 +1,6 @@
 package com.example.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayRoutesConfig {
 
+  @Value("${zylo.endpoints.integrated-services.uri}")
+  private String integratedServiceUri;
+
   @Bean
   public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-    //return builder.routes()
-    //    .route("integrated-services", r -> r
-    //        .path("/auth/login")
-    //        //.filters(f -> f.filter())
-    //    )
-    //    .build();
-    return null;
+    return builder.routes()
+        .route("integrated-services", r -> r
+            .path("/v1/login")
+            .filters(f -> f.rewritePath("/v1/login", "/auth/login"))
+            .uri(integratedServiceUri)
+        )
+        .route("integrated-services/user/all", r -> r
+            .path("/v1/user/all")
+            .filters(f -> f.rewritePath("/v1/user/all", "/user/all"))
+            .uri(integratedServiceUri))
+        .build();
   }
 }
