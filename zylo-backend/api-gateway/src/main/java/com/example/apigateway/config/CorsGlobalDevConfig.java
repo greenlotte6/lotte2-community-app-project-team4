8d0607106e1a9e076c2a6d8b9785724d9e148ce7
id3ntity99 <jas06113@gmail.com>
2025-06-12 15:23:57 +0900

    CORS 설정

---
+++ zylo-backend/api-gateway/src/main/java/com/example/apigateway/config/CorsGlobalDevConfig.java
@@ -0,0 +1,28 @@
+package com.example.apigateway.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@Profile("dev")
+public class CorsGlobalDevConfig {
+
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("https://your-frontend.com"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsWebFilter(source);
+  }
+}
---
+++ zylo-backend/api-gateway/src/main/java/com/example/apigateway/filter/drive/DriveUploadFilter.java
@@ -0,0 +1,16 @@
+package com.example.apigateway.filter.drive;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class DriveUploadFilter implements GatewayFilter {
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    return null;
+  }
+}
