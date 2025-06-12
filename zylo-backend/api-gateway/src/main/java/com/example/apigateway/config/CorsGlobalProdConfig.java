55692176eea15b946f4dbafbdc917bac3723e532
id3ntity99 <jas06113@gmail.com>
2025-06-12 15:28:55 +0900

    배포 환경에서의 CORS 설정

---
+++ zylo-backend/api-gateway/src/main/java/com/example/apigateway/config/CorsGlobalProdConfig.java
@@ -0,0 +1,25 @@
+package com.example.apigateway.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+public class CorsGlobalProdConfig {
+
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("https://greenlotteon.com"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsWebFilter(source);
+  }
+
+}
