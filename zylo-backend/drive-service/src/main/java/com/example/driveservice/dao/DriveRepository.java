df576f02cc69771a44fcfaf5a5ab219ac912b563
id3ntity99 <jas06113@gmail.com>
2025-06-13 14:49:38 +0900

    중복 파일 업로드 기능 추가

--- zylo-backend/drive-service/src/main/java/com/example/driveservice/dao/DriveRepository.java
+++ zylo-backend/drive-service/src/main/java/com/example/driveservice/dao/DriveRepository.java
@@ -17,19 +17,41 @@ public class DriveRepository {
   private final MongoTemplate template;
 
   public boolean usernameExists(String username) {
-    Query query = new Query(Criteria.where("username").is(username));
+    Query query = new Query(Criteria.where("uid").is(username));
     return template.exists(query, UploadsDocument.class);
   }
 
+  public UploadsDocument fileExists(String username, String filename) {
+    Query query = new Query(
+        Criteria.where("uid").is(username).and("files.filename").is(filename));
+    return template.findOne(query, UploadsDocument.class);
+  }
+
   public void save(String username, FileDocument fileDocument) {
-    //TODO: Update current capacity
+    // TODO: 중복되는 filename을 가진 파일은 업데이트만 하기
     long currentCapacity = findCurrentCapacity(username);
-    long updatedCapacity = currentCapacity + fileDocument.getSize();
-    Query query = new Query(Criteria.where("uid").is(username));
-    Update update = new Update().setOnInsert("uid", username)
-        .push("files", fileDocument)
-        .setOnInsert("currentCapacity", updatedCapacity);
-    template.upsert(query, update, UploadsDocument.class);
+    UploadsDocument uploadList = fileExists(username, fileDocument.getFilename());
+    if (uploadList != null) { //같은 이름의 파일이 있는 경우
+      FileDocument originalFile = uploadList.getFiles().get(0);
+      long originalSize = originalFile.getSize(); // 기존 파일의 크기 가져오기
+      long newSize = fileDocument.getSize(); // 새로 업로드 할 파일의 크기
+      long updatedSize = currentCapacity - originalSize + newSize;
+      Query query = new Query(
+          Criteria.where("uid").is(username).and("files.filename").is(fileDocument.getFilename()));
+      Update update = new Update()
+          .set("currentCapacity", updatedSize)
+          .set("files.$.uploadPath", fileDocument.getUploadPath())
+          .set("files.$.size", fileDocument.getSize())
+          .set("files.$.uploadDate", fileDocument.getUploadDate());
+      template.updateFirst(query, update, UploadsDocument.class);
+    } else {
+      long updatedCapacity = currentCapacity + fileDocument.getSize();
+      Query query = new Query(Criteria.where("uid").is(username));
+      Update update = new Update().setOnInsert("uid", username)
+          .push("files", fileDocument)
+          .set("currentCapacity", updatedCapacity);
+      template.upsert(query, update, UploadsDocument.class);
+    }
   }
 
   public long findCapacity(String username) throws IllegalUsernameException {
