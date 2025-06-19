5f8a8e1ac217003c37acaef85f2e76896c1af713
id3ntity99 <jas06113@gmail.com>
2025-06-19 17:29:37 +0900

    가상 FS 테스트 클래스

---
+++ zylo-backend/drive-service/src/test/java/com/example/driveservice/InMemoryFileSystemTest.java
@@ -0,0 +1,13 @@
+package com.example.driveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryFileSystemTest {
+
+  @Test
+  void test() {
+
+  }
+}
