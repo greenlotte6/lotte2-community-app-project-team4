c6b16343175efb1e107a8aa56fc77a8f1127fca6
id3ntity99 <jas06113@gmail.com>
2025-06-19 17:31:54 +0900

    Deleted builder annotation

--- zylo-backend/drive-service/src/main/java/com/example/driveservice/document/Node.java
+++ zylo-backend/drive-service/src/main/java/com/example/driveservice/document/Node.java
@@ -2,14 +2,12 @@ package com.example.driveservice.document;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class Node {
